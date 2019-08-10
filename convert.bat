0bc5cefc10e3697a0c1307462b2ca11c27db09a0
Kenta Kabashima (Windows on Home PC) <kenta_program37@hotmail.co.jp>
2019-08-10 12:39:43 +0900

    Make jpg file addtionally

--- convert.bat
+++ convert.bat
@@ -5,5 +5,6 @@ magick convert -density 1000 KIcon.svg -resize 48x48 -transparent white PNG/KIco
 magick convert -density 1000 KIcon.svg -resize 64x64 -transparent white PNG/KIcon64.png
 magick convert -density 1000 KIcon.svg -resize 152x152 -transparent white PNG/KIcon152.png
 magick convert -density 1000 KIcon.svg -resize 512x512 -transparent white PNG/KIcon512.png
-magick convert -density 1000 KIcon.svg -resize 1024x1024 -transparent white PNG/KIcon1024.png
-magick convert PNG/KIcon16.png PNG/KIcon24.png PNG/KIcon32.png PNG/KIcon48.png PNG/KIcon64.png PNG/KIcon152.png PNG/KIcon512.png PNG/KIcon1024.png -colors 256 KIcon.ico
+magick convert PNG/KIcon16.png PNG/KIcon24.png PNG/KIcon32.png PNG/KIcon48.png PNG/KIcon64.png PNG/KIcon152.png PNG/KIcon512.png -colors 256 KIcon.ico
+
+magick convert -density 1000 KIcon.svg -resize 512x512 -transparent white KIcon.jpg
